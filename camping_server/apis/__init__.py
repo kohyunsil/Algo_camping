e17541d426cc8740b1a1ac4d5d2f774e64a835b4
HyunjinKIM-Chloe <a83628670@gmail.com>
2021-05-30 13:20:07 +0900

    Revert "Add: gocamping api"
    This reverts commit e6ecc7b1f517cac0c221b3f78e3f1236828a1b72.

--- camping_server/apis/__init__.py
+++
@@ -1,9 +0,0 @@
-import gocamping_api as ga
-import pandas as pd
-pd.set_option('display.max_row', 500)
-pd.set_option('display.max_columns', 100)
-
-if __name__ == '__main__':
-    s = ga.gocampingApi()
-    s.get_secretKey()
-    s.gocampingAPI()
