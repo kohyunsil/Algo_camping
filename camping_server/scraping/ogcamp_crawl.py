3304f7a7b81fe9a8361661ae347e595da0e33d9b
kohyunsil <rh523@naver.com>
2021-06-07 06:57:02 +0900

    Modify ogcamp scraping code

--- camping_server/scraping/ogcamp_crawl.py
+++ camping_server/scraping/ogcamp_crawl.py
@@ -28,6 +28,7 @@ class OgcampScraping:
         submit.click()
         time.sleep(2)
 
+
         url_links = []
         for i in tqdm(range(1,19)):
             self.driver.get(f"https://www.5gcamp.com/?c=5g&p={i}&campID=2601")
@@ -37,12 +38,15 @@ class OgcampScraping:
 
         datas = []
         for link in tqdm(url_links):
-            time.sleep(2)
+            time.sleep(1)
             self.driver.get(link)
             try:
                 title = self.driver.find_element_by_css_selector('#campcontents > div.viewheader > h3').text
-                addr = self.driver.find_element_by_css_selector('#vContent > h4.chead.address.first.fblack > a.clipboardCopy').text
-                lat = self.driver.find_element_by_css_selector('#vContent > p > em').text
+                addr = self.driver.find_element_by_css_selector(
+                    '#vContent > h4.chead.address.first.fblack > a.clipboardCopy').text
+                lats = self.driver.find_element_by_css_selector('#vContent > p > em').text
+                lat = lats.split('경도')[0].strip()
+                lon = '경도' + lats.split('경도')[1].strip()
                 envs = self.driver.find_elements_by_css_selector('#vContent > div.facilities > div > div')
                 env = [env.find_element_by_css_selector("p.f_name").text for env in envs]
                 en = ', '.join(env)
@@ -50,25 +54,29 @@ class OgcampScraping:
                 photoss = self.driver.find_elements_by_css_selector('#vContent > div.photos > div')
                 photos = [photos.find_element_by_css_selector('img').get_attribute('src') for photos in photoss]
                 photo = ', '.join(photos)
-                time.sleep(1)
-                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                click_review = self.driver.find_element_by_xpath('//*[@id="moreComment"]/a').click()
-                time.sleep(2)
-                reviews = self.driver.find_element_by_id("clist").text
+                # time.sleep(1)
+                # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # click_review = self.driver.find_element_by_xpath('//*[@id="moreComment"]/a').click()
+                # time.sleep(2)
+                # reviews = self.driver.find_element_by_id("clist").text
             except NoSuchElementException:
-                pass
+                lat = "정보없음"
+                lon = "정보없음"
+                en = "정보없음"
+                desc = "정보없음"
+                photo = "정보없음"
+
             data = {
                 "title": title,
                 "addr": addr,
-                "lat" : lat.split('경도')[0].strip(),
-                "lon" : '경도' + lat.split('경도')[1].strip(),
-                "environment" : en,
-                "desc" : desc,
-                "photo" : photo,
-                "reviews" : reviews
+                "lat": lat,
+                "lon": lon,
+                "environment": en,
+                "desc": desc,
+                "photo": photo,
             }
             datas.append(data)
 
         self.df = pd.DataFrame(datas)
-        self.df.to_csv(constant.PATH + 'ogcamp.csv', index=False, encoding="utf-8-sig")
+        self.df.to_csv(constant.PATH + 'ogcamp.csv', encoding="utf-8-sig")
         self.driver.quit()
