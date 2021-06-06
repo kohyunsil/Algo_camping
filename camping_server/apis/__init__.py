f62d1cff369ef77098923063e35b64833468f1eb
hyunjinkim <a83628670@gmail.com>
2021-06-07 00:22:48 +0900

    Modify: add API crawling code (tour spot estimated congestion level)

--- camping_server/apis/__init__.py
+++ camping_server/apis/__init__.py
@@ -5,9 +5,8 @@ pd.set_option('display.max_row', 500)
 pd.set_option('display.max_columns', 100)
 
 if __name__ == '__main__':
-    s0 = ga.GocampingApi()
-    s0.gocampingAPI()
+    # s0 = ga.GocampingApi()
+    # s0.gocampingAPI()
 
-    # s1 = ka.KoreaTourApi()
-    # s1.tourspotAPI(i, contentTypeId, 1000) # def docsring 참조
-    # s1.festivalAPI(20210601) #축제 시작일 기준 설정: YYYYMMDD
+    s1 = ka.KoreaTourApi()
+    s1.tour_estiDecoAPI(20210701, 20210707)
