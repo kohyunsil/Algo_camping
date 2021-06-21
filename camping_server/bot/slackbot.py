ea43aa0a3d1c2428ec5fee8226ba3e0a789e4813
SeyoungKo <seyoungg1223@gmail.com>
2021-06-22 02:44:43 +0900

    Delete : invalid branch files

--- camping_server/bot/slackbot.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-import camping_server.config as config
-
-class IncomingWebhook:
-    def send_msg(err_msg):
-        payload = {"channel": "dss17", "username": "bot", "text": err_msg}
-        response = requests.post(config.Config.WEBHOOK_URL, json.dumps(payload))
-        print(response)
