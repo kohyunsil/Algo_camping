3c749a9c58980bed057c14dcad89ec621e31f7fd
hyunjinkim <a83628670@gmail.com>
2021-07-01 01:03:11 +0900

    Add: kakao&naver concat review data clustering
    w2v vector size 에 따라 200d, 2d 로 나누어 작업

--- camping_modeling/review_nlp/__init__.py
+++ camping_modeling/review_nlp/__init__.py
@@ -5,8 +5,9 @@ import category_modeling as cm
 if __name__ == '__main__':
     s0 = nlp.Review_nlp()
     s1 = cm.CategoryModel()
-    s0.vectorizing()
-    s0.load_model()
+    # s0.vectorizing()
+    # s0.load_model()
+    s0.rv_tokenizing('kakao_review_cat_predict', 'contents')
 
     # s1.make_cat_predictor()
     # s1.apply_cat_predictor()
