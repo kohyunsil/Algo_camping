37ed91d8d3185bd098f8deef73a1210a1d442193
hyunjinkim <a83628670@gmail.com>
2021-07-06 17:15:09 +0900

    Add: kakao reviews category predicted by naver classifier
    naver classifier is made by pipelines(tfidf transformer+svm model) whose accuracy 83%
    kakao reviews are devided by sentences before prediction for better prediction efficiency.

--- camping_modeling/review_nlp/category_modeling.py
+++ camping_modeling/review_nlp/category_modeling.py
@@ -13,19 +13,22 @@ class CategoryModel:
     def __init__(self):
         pass
 
-    def make_cat_predictor(self):
+    def make_cat_predictor(self, dataname, colname, labelname):
         # get tokenized data
         s = nlp.Review_nlp()
-        o_data = s.rv_tokenizing('v5_category_re', 'highlight_review')
-        data = o_data[['highlight_review', 'category']]
+        o_data = s.rv_tokenizing(dataname, colname)
+        # o_data = s.read_file("review_hdbscan")
+        print("Data loaded")
+        data = o_data[[f'{colname}', f'{labelname}']]
         data.dropna(axis=0, inplace=True)
 
         train_x, test_x, train_y, test_y = train_test_split(
-            data.highlight_review, data.category, test_size=0.2, random_state=0,
-            stratify=data.category
+            data[f'{colname}'], data[f'{labelname}'], test_size=0.2, random_state=0,
+            stratify=data[f'{labelname}']
         )
 
         # 모델 학습
+        print("Model learning started")
         clf = Pipeline([
             ('vect', CountVectorizer()),
             ('tfdif', TfidfTransformer()),
@@ -35,21 +38,21 @@ class CategoryModel:
         model = clf.fit(train_x, train_y)
         print("모델 정확도: ", np.round(model.score(test_x, test_y), 4) * 100)
 
-        with open("../models/model2.pkl", "wb") as file:
+        with open(f"../models/{dataname}_cls.pkl", "wb") as file:
             pickle.dump(model, file)
         print("Model saved!")
 
-    def apply_cat_predictor(self):
-        with open("../models/model2.pkl", "rb") as file:
+    def apply_cat_predictor(self, modelname, newfile, colname):
+        with open(f"../models/{modelname}.pkl", "rb") as file:
             load_model = pickle.load(file)
 
-        kakao = pd.read_csv("../../datas/kakao_camping_review_revised.csv", encoding='utf-8-sig')
+        newfile = pd.read_csv(f"../../datas/{newfile}.csv", encoding='utf-8-sig')
         cat_list = []
-        for k in kakao['contents']:
+        for k in newfile[f'{colname}']:
             try:
                 result = load_model.predict([k])[0]
             except:
                 result = ""
             cat_list.append(result)
-        kakao['category'] = cat_list
-        kakao.to_csv("../../datas/kakao_review_cat_predict.csv", encoding='utf-8-sig')
+        newfile['category'] = cat_list
+        newfile.to_csv(f"../../datas/{modelname}_cat_predict.csv", encoding='utf-8-sig')
