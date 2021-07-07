a311ce3d689d462f7fa2a8b16b4b688de3fa2480
hyunjinkim <a83628670@gmail.com>
2021-07-07 11:13:46 +0900

    Modify: Calc logic class (binary, percentile)

--- camping_modeling/algostar/cat_points.py
+++ camping_modeling/algostar/cat_points.py
@@ -4,12 +4,23 @@ import numpy as np
 class Calc_logic:
     def __init__(self):
         pass
-    def binary_calc(self, data, colname, con1, h_point, l_point):
-        """값의 유무에 따라 0 또는 1점 부여하는 이진 점수화
-        h_point에 높은 점수, l_point에 낮은 점수 기입"""
-        target_df = pd.DataFrame(data[f'{colname}'].isnull() == True)
-        target_df[f'{colname}'] = [l_point if r == True else h_point for r in target_df[f'{colname}']]
-        target_df
+
+    def binary_calc(data, colname, h_point, l_point, h_con):
+        """
+        특정 조건에 따라 True/False 이진 점수화
+        h_point에 높은 점수, l_point에 낮은 점수 기입, h_point 를 받는 조건을 h_con 에 기입
+        (1) h_con = int/float (ex: 3 이상 : 1점 / 3 미만 : 0점)
+        (2) h_con = 'notnull' (ex: 번호 있으면 1점 / 없으면 0점)
+        (3) h_con = f'{string}' (ex: 직영 1점 / 위탁 0점 -> string='직영')
+        """
+        if type(h_con) != str:
+            target_df = pd.DataFrame(data[f'{colname}'] >= h_con)
+        elif h_con == 'notnull':
+            target_df = pd.DataFrame(data[f'{colname}'].isnull() == False)
+        else:
+            target_df = pd.DataFrame(data[f'{colname}'] == h_con)
+        target_df[f'{colname}'] = [h_point if r == True else l_point for r in target_df[f'{colname}']]
+        return target_df
 
     def cls_calc(self, data, colname, c1, c2, c3, p1, p2, p3):
         """3개의 분류로 점수 부여
@@ -18,7 +29,9 @@ class Calc_logic:
 
     def percent_calc(self, data, colname):
         """percentile 기준 백분율 점수"""
-
+        target_df = pd.DataFrame(data[f'{colname}'])
+        target_df[f'{colname}'] = target_df[f'{colname}'].rank(pct=True)
+        return target_df
 
 
 class Cat5_points:
@@ -26,7 +39,12 @@ class Cat5_points:
         self.path = "../../datas/"
         self.camp_df = pd.read_csv(self.path+"camp_data_merge.csv", encoding="utf-8-sig")
         self.review_df = pd.read_csv(self.path+"review_final_point.csv", encoding="utf-8-sig")
-        self.weights = pd.read_csv(self.path+"cat_weights.csv", encoding="utf-8-sig")
+        self.weights = pd.read_excel(self.path+"cat_weights.xlsx")
+
+    def get_weights(self, category, origindf, colname):
+        df = self.weights
+        weights = df[df['category']==category][df['origindf']==origindf][df['colname']==colname]['weights']
+        return float(weights)
 
     def comfort_point(self):
         comfort_df = camp_df[['insrncAt','trsagntNo','mangeDivNm','manageNmpr','sitedStnc','glampInnerFclty',
