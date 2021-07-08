9302bed3487dd5adeed498b7c2ce695c4aa9ece6
hyunjinkim <a83628670@gmail.com>
2021-07-08 15:46:33 +0900

    Modify: updated category points calculation func

---
+++ camping_modeling/algostar/__init__.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import cat_points as cp
+
+if __name__ == '__main__':
+    # cp.percent_calc()
+    # cls_calc(data, colname, c1, c2, c3, p1, p2, p3)
+    cat5 = cp.Cat5_points()
+    cat5.together_point('(주)데일리랜드')
--- camping_modeling/algostar/cat_points.py
+++ camping_modeling/algostar/cat_points.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
 
 class Calc_logic:
     def __init__(self):
@@ -26,8 +29,8 @@ class Calc_logic:
         """
         3개의 분류로 점수 부여
         condition(c1, c2, c3) type이 string / int 인지 파악 후 조건 함수 작성
-        manageNmpr  int     /     3명(c1) 이상: 10점(p1)/ 2명이하(c2): 5점(p2)/ 0은(c3) 0점(p3)
-        brazierCl   str     /      개별(c1) 2점(p1) / 공동취사장(c2) 1점(p2)/ 불가(c3) 0점(p3)
+        manageNmpr  int     / 3명(c1) 이상: 10점(p1)/ 2명이하(c2): 5점(p2)/ 0은(c3) 0점(p3)
+        brazierCl   str     / 개별(c1) 2점(p1) / 공동취사장(c2) 1점(p2)/ 불가(c3) 0점(p3)
         """
 
         if type(c1) != str:
@@ -38,7 +41,6 @@ class Calc_logic:
                     return p2
                 else:
                     return p3
-
             target_df = pd.DataFrame(data[f'{colname}'])
             target_df[f'{colname}'] = target_df[f'{colname}'].apply(lambda x: func(x))
 
@@ -63,26 +65,64 @@ class Calc_logic:
         return target_df
 
 
-class Cat5_points:
+class Calc_weights:
     def __init__(self):
         self.path = "../../datas/"
-        self.camp_df = pd.read_csv(self.path+"camp_data_merge.csv", encoding="utf-8-sig")
-        self.review_df = pd.read_csv(self.path+"review_final_point.csv", encoding="utf-8-sig")
-        self.weights = pd.read_excel(self.path+"cat_weights.xlsx")
+        self.weights = pd.read_excel(self.path + "cat_weights.xlsx")
+        self.df = pd.read_csv(self.path+"algo_merge_result.csv", encoding="utf-8-sig")
 
     def get_weights(self, category, origindf, colname):
         df = self.weights
         weights = df[df['category']==category][df['origindf']==origindf][df['colname']==colname]['weights']
         return float(weights)
 
-    def comfort_point(self):
-        comfort_df = camp_df[['insrncAt','trsagntNo','mangeDivNm','manageNmpr','sitedStnc','glampInnerFclty',
-                              'caravInnerFclty','trlerAcmpnyAt','caravAcmpnyAt','trlerAcmpnyAt','caravAcmpnyAt',
-                              'toiletCo','swrmCo','wtrplCo','brazierCl','sbrsCl','sbrsEtc','posblFcltyCl',
-                              'extshrCo','frprvtWrppCo','frprvtSandCo','fireSensorCo']].copy()
+    def get_cat_df(self, category):
+        cp_df = self.df[self.get_col_list(category, 'camp').append('camp')]
+        rv_df = self.df[self.get_col_list(category, 'review').append('camp')]
+        return cp_df, rv_df
 
-    def together_point(self):
+    def get_col_list(self, category, origindf=None):
+        df = self.weights
+        if origindf == None:
+            result = df[df['category'] == category]['colname']
+        else:
+            result = df[df['category']==category][df['origindf']==origindf]['colname']
+        return result.tolist()
+
+    def weights_list(self, category, origindf):
+        weights_list = [self.get_weights(category, origindf, a) for a in self.get_col_list(category, origindf)]
+        return weights_list
+
+class Cat5_points(Calc_logic, Calc_weights):
+
+    def __init__(self):
+        super().__init__()
+        self.path = "../../datas/"
+        self.weights = pd.read_excel(self.path + "cat_weights.xlsx")
+        self.df = pd.read_csv(self.path+"algo_merge_result.csv", encoding="utf-8-sig")
+        self.camp_df = pd.read_csv(self.path+"camp_algo_merge.csv", encoding="utf-8-sig")
+        self.review_df = pd.read_csv(self.path+"review_final_point.csv", encoding="utf-8-sig")
+
+
+    def comfort_point(self):
         pass
+
+    def together_point(self, campname, category='together'):
+        cp_df, rv_df = self.get_cat_df(category)
+        rv_col_ls, cp_col_ls = self.get_col_list(category, 'review'), self.get_col_list(category, 'camp')
+        result = 0
+        print(rv_col_ls)
+        for colname in rv_col_ls:
+            weight = self.get_weights(category, 'review', colname)
+            point = rv_df[rv_df['camp']==campname][rv_df['category']==colname]['final_point']
+            point = float(point).round(1)
+            result += weight * point
+        rv_points = result
+
+        print(rv_points)
+
+
+
     def fun_point(self):
         pass
     def healing_point(self):
