350e2d7b53064944ac925f4730724d0c2d66c6a5
SeyoungKo <seyoungg1223@gmail.com>
2021-07-09 03:17:25 +0900

    Modify : wsgi server
    웹서버 통신을 위한 호출 시점 변경

--- camping_server1/app.py
+++ camping_server1/app.py
@@ -1,1 +1,5 @@
+from gevent.pywsgi import WSGIServer
 from app import app
+
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
--- camping_server1/app/__init__.py
+++ camping_server1/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import *
-import configparser
 from app.config import Config
 from flask_restx import Api
-from flask_sqlalchemy import SQLAlchemy
-
-import pymysql
 import warnings
+
 warnings.filterwarnings('ignore')
 
 blueprint = Blueprint('api', __name__)
@@ -17,6 +14,5 @@ api = Api(blueprint,
 app = Flask(__name__)
 # api = Api(app)
 app.config.from_object(Config)
-
 
 from app.view import routes, apis
