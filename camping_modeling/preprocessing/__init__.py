ac85504e923dc8cba683c16c4980fdc0d8ff44dd
kohyunsil <rh523@naver.com>
2021-07-14 14:59:27 +0900

    Modify : camp api data & crawling data convert english columns

--- camping_modeling/preprocessing/__init__.py
+++ camping_modeling/preprocessing/__init__.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import kakao_review_nlp as nlp
 import category_modeling as cm
+import camp_api_crawling_merge as cacm
 
 if __name__ == '__main__':
-    s0 = nlp.Review_nlp()
-    s1 = cm.CategoryModel()
+    # s0 = nlp.Review_nlp()
+    # s1 = cm.CategoryModel()
     # s0.vectorizing()
     # s0.load_model()
     # s0.rv_tokenizing('kakao_review_cat_predict', 'contents')
 
     # s1.make_cat_predictor('camp_description', 'full_text', 'labels')
-    s1.apply_cat_predictor('model2', 'kakao_review_sent', 'review')
+    # s1.apply_cat_predictor('model2', 'kakao_review_sent', 'review')
+
+    # merge
+    c = cacm.CampMerge()
+    grm = cacm.GocampReviewMerge()
+    c.camp_api_data_merge('camp_api_info_210619', 'camp_crawl_links')
+    grm.review_camp_merge('camp_algo_merge', 'v5_category_re', 'kakao_review_cat_revised')
--- camping_modeling/preprocessing/camp_api_crawling_merge.py
+++ camping_modeling/preprocessing/camp_api_crawling_merge.py
@@ -2,6 +2,23 @@ import re
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler, MaxAbsScaler
 
+# 한글 폰트 설정
+import matplotlib.pyplot as plt
+import platform
+from matplotlib import font_manager, rc
+import matplotlib.pyplot as plt
+plt.rcParams['axes.unicode_minus'] = False
+
+if platform.system() == 'Windows':
+    path = "c:/Windows/Fonts/malgun.ttf"
+    font_name = font_manager.FontProperties(fname=path).get_name()
+    rc('font', family=font_name)
+elif platform.system() == 'Darwin':
+    rc('font', family='AppleGothic')
+elif platform.system() == 'Linux':
+    rc('font', family='NanumBarunGothic')
+else:
+    print('Unknown system... sorry~')
 
 class CampMerge:
 
@@ -62,11 +79,16 @@ class CampMerge:
             col_count(i)
 
         camp_algo_merge = camp_algo_merge.rename(columns={'facltNm':'camp'})
+        camp_algo_merge = camp_algo_merge.rename(
+            columns={'사이트간격이넓은': '사이트 간격이 넓은', '온수잘나오는': '온수 잘 나오는', '차대기편한': '차대기 편한',
+                     '아이들놀기좋은': '아이들 놀기 좋은', '자전거타기좋은': '자전거 타기 좋은', '별보기좋은': '별 보기 좋은',
+                     '수영장있는': '수영장 있는', '물놀이하기좋은': '물놀이 하기 좋은', '그늘이많은': '그늘이 많은', '바다가보이는': '바다가 보이는'})
+
         camp_algo_merge.to_csv('../datas/camp_algo_merge.csv', index=False, encoding='utf-8-sig')
 
 
 
-class GocampReviewMerge(CampMerge):
+class GocampReviewMerge():
 
     def review_camp_merge(self, api_data, naver_review_file, kakao_review_file):
         """ 카카오 데이터는 네이버 카테고리 학습 후 반영"""
@@ -144,13 +166,15 @@ class GocampReviewMerge(CampMerge):
         result1 = merge_result.iloc[:, 44:].fillna(0)
         result2 = merge_result.iloc[:, :44]
         algo_result = pd.concat([result2, result1], 1)
+        algo_result = algo_result.rename(columns={'사이트 간격이 넓은':'spacious_s', '깨끗한':'clean_s', '온수 잘 나오는':'hot_water_s',
+        '차대기 편한':'parking_s', '아이들 놀기 좋은':'with_child_s', '생태교육':'ecological_s', '문화유적':'cultural_s', '축제':'festival_s',
+        '둘레길':'trail_s', '자전거 타기 좋은':'bicycle_s', '별 보기 좋은':'star_s', '힐링':'healing_s', '커플':'with_couple_s', '가족':'with_family_s',
+        '수영장 있는':'pool_s', '계곡옆':'valley_s', '물놀이 하기 좋은':'waterplay_s', '물맑은':'pure_water_s', '그늘이 많은':'shade_s',
+        '바다가 보이는':'ocean_s', '익스트림':'extreme_s', '가격':'price_r', '만족도':'satisfied_r', '맛':'taste_r', '메인시설':'main_r','목적':'object_r',
+        '부대/공용시설': 'facility_r', '분위기':'atmos_r', '비품':'equipment_r','서비스':'service_r', '수영장':'pool_r',
+        '시설물관리':'manage_r', '아이 만족도':'childlike_r', '예약':'reservation_r', '와이파이':'wifi_r','위치':'location_r', '음식/조식':'food_r',
+        '입장': 'enter_r', '전망':'view_r', '주차':'parking_r', '즐길거리': 'exciting_r', '청결도': 'clean_r', '편의/부대시설':'conv_facility_r', '혼잡도': 'congestion_r'})
+        # algo_result.set_index('camp', inplace=True)
 
-        algo_result.to_csv('algo_merge_result.csv', encoding='utf-8-sig', index=False)
 
-
-
-if __name__  == '__main__':
-    c = CampMerge()
-    grm = GocampReviewMerge()
-    # c.camp_api_data_merge('camp_api_info_210619', 'camp_crawl_links')
-    grm.review_camp_merge('camp_algo_merge', 'v5_category_re', 'kakao_review_cat_revised')
+        algo_result.to_csv('../datas/algo_merge_result.csv', encoding='utf-8-sig', index=False)
