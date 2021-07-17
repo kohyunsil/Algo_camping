9fbe5acab635bf28a3fc511c8145541637001d13
hyunjinkim <a83628670@gmail.com>
2021-07-17 13:57:23 +0900

    Delete: server1 unnecessary files

--- camping_server1/app/util/user_dto.py
+++
@@ -1,9 +0,0 @@
-from flask_restx import Namespace, fields
-
-class UserDTO:
-    api = Namespace('user', description='relating to user')
-    user = api.model('user', {
-        'user_name': fields.String(required=False),
-        'email': fields.String(required=False),
-    })
-
