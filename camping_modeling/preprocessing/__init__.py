efd5d4599a4ec076369b621c58155c00fb1f4141
hyunjinkim <a83628670@gmail.com>
2021-08-05 18:24:12 +0900

    Modify: kakao review category predicting
    kk_apply_cat_predictor 를 실행하면 카테고리가 추정된 kakao 리뷰 데이터 return

--- camping_modeling/preprocessing/__init__.py
+++ camping_modeling/preprocessing/__init__.py
@@ -4,10 +4,10 @@ import camp_api_crawling_merge as cacm
 import tag_prior as tp
 
 if __name__ == '__main__':
-    # s1 = cm.CategoryModel()
+    s1 = cm.CategoryModel()
     # s1.rv_cleansing('highlight_review', 'nv')
     # s1.make_cat_predictor('highlight_review', 'category')
-    # s1.kk_apply_cat_predictor('review', make_cat_predictor=True)
+    s1.kk_apply_cat_predictor('review', make_cat_predictor=False)
 
     # s1.make_cat_predictor('camp_description', 'full_text', 'labels')
     # s1.apply_cat_predictor('model2', 'kakao_review_sent', 'review')
@@ -24,5 +24,5 @@ if __name__ == '__main__':
 
 
     #tag-merge
-    tag_df = tp.TagMerge()
-    tag_df.tag_merge()
+    # tag_df = tp.TagMerge()
+    # tag_df.tag_merge()
--- camping_modeling/preprocessing/review_category_modeling.py
+++ camping_modeling/preprocessing/review_category_modeling.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
 import pickle
+from tqdm import tqdm
+import re
 import kss
 from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from konlpy.tag import Okt
 from sklearn.pipeline import Pipeline
+import warnings
 import config as config
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 
 class CategoryModel:
@@ -16,9 +20,11 @@ class CategoryModel:
         self.kk_data = config.Config.KK_DATA
         self.nv_data = config.Config.NV_DATA
 
-    def rv_cleansing(self, target_col, filename='nv'):
+    def rv_cleansing(self, target_col, filename):
         if filename == 'nv':
             data = self.nv_data
+        elif filename == 'kk':
+            data = self.kk_data
         else:
             data = pd.read_csv(self.path+f"{filename}.csv", encoding='utf-8-sig', low_memory=False)
 
@@ -27,13 +33,13 @@ class CategoryModel:
         result_list = []
         english = re.compile(r'[a-zA-Z]')
 
-        for i in range(len(reviews)):
+        for i in tqdm(range(len(reviews))):
             rv = str(reviews[i])
 
             if len(rv) > 0:
-                rv = re.sub(english,"",rv)
+                rv = re.sub(english, "", rv)
                 a = re.sub(' ', 's', rv)  # 띄어쓰기 위해 s 붙힘
-                b = okt.pos(a, stem = True, norm= True)
+                b = okt.pos(a, stem=True, norm=True)
                 string = ''
                 for word in b:  # tokenize str version
                     string += word[0]
@@ -42,10 +48,12 @@ class CategoryModel:
             else:
                 result_list.append("")
         data[f"{target_col}"] = result_list
+        print(data[f"{target_col}"][:10])
+        print(f"---- {filename} cleansing succeed! ----")
         return data
 
     def kk_sent_sep(self):
-        kk = self.kk_data
+        kk = self.rv_cleansing('contents', 'kk')
         kk = kk[['contents', 'place_name', 'point']]  # , 'username', 'date']]
         camp_star = kk.groupby('place_name').mean().round(3)
 
@@ -74,9 +82,9 @@ class CategoryModel:
 
     def make_cat_predictor(self, target_col, label_name):
         # get tokenized data
-        o_data = self.rv_cleansing('nv')
+        o_data = self.rv_cleansing(target_col, 'nv')
         print("Data loaded")
-        data = o_data[[f'{target_col}', f'{label_name}']]
+        data = o_data[[f'{target_col}', f'{label_name}']].copy()
         data.dropna(axis=0, inplace=True)
 
         train_x, test_x, train_y, test_y = train_test_split(
@@ -94,19 +102,24 @@ class CategoryModel:
         model = clf.fit(train_x, train_y)
         print("모델 정확도: ", np.round(model.score(test_x, test_y), 4) * 100)
 
-        with open(f"../models/cat_cls.pkl", "wb") as file:
+        with open(f"../models/rv_cat_cls.pkl", "wb") as file:
             pickle.dump(model, file)
         print("Model saved!")
         return model
 
-    def kk_apply_cat_predictor(self, target_col, make_cat_predictor=True):
+    def kk_apply_cat_predictor(self, target_col, make_cat_predictor=False):
+        """ make_cat_predictor=False
+            : 기존에 저장되어있는 네이버 카테고리 분류기 rv_cat_cls.pkl 를 불러와서 예측
+            make_cat_predictor=True
+            : 새롭게 네이버 카테고리 분류기 rv_cat_cls.pkl 를 만든 후 불러와서 예측 (약 15분 소요)"""
         kk_cat = self.kk_sent_sep()
 
-        if make_cat_predictor:
-            load_model = self.make_cat_predictor('highlight_review', 'category')
-        else:
-            with open(f"../models/cat_cls.pkl", "rb") as file:
+        if make_cat_predictor is False:
+            with open(f"../models/rv_cat_cls.pkl", "rb") as file:
                 load_model = pickle.load(file)
+        else:
+            load_model = self.make_cat_predictor('highlight_review', 'category')
+        print("Model loaded")
 
         cat_list = []
         for k in kk_cat[f'{target_col}']:
