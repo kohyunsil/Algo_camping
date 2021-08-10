93e4727cb946ebeda2d2745265d65bd00470f7ec
hyunjinkim <a83628670@gmail.com>
2021-08-10 17:05:05 +0900

    Modify: camp_api_crawling code move dir from preprocessing  to algostar

--- camping_modeling/preprocessing/config.py
+++ camping_modeling/preprocessing/config.py
@@ -5,10 +5,6 @@ from datetime import datetime
 class Config:
     # 경로 및 파일 불러오기
     PATH = "../../datas/"
-    API_DATA = pd.read_csv(PATH + "camp_api_info_210619.csv", encoding="utf-8-sig")
-    CRAWL_DATA = pd.read_csv(PATH + "camp_crawl_links.csv", encoding='utf-8-sig')
-    KAKAO = pd.read_csv(PATH + "kk_cat_predict_0805_181611.csv", encoding='utf-8-sig')
-
     NV_DATA = pd.read_csv(PATH + "v5_category_re.csv", encoding='utf-8-sig')
     KK_DATA = pd.read_csv(PATH + "kakao_camping_review_revised.csv", encoding='utf-8-sig')
 
